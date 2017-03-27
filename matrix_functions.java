fc57ff2b90ee43125a99e146ff305e197729f0ff
jwu777 <j295w301@ku.edu>
2017-03-27 14:07:11 -0500

    Constructor method implemented

--- matrix_functions.java
+++ matrix_functions.java
@@ -3,4 +3,23 @@
 
 public class matrix_functions
 {
+  /**
+	Variables
+  **/
+	int size = 0;
+  
+	double[][] matrixA;
+  
+  /**
+	Constructor method
+  **/
+	
+	// Constructor method
+	// Accepts a square matrix of size n
+	public matrix_functions(int n)
+	{
+		size = n;
+		
+		matrixA = new double[size][size];
+	}
 }
