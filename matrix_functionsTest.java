10d2129d67dc514db4cc7e1785b62ef846e3e571
jwu777 <j295w301@ku.edu>
2017-03-27 14:58:12 -0500

    Updated to functional test methods/cases

--- matrix_functionsTest.java
+++ matrix_functionsTest.java
@@ -1,4 +1,3 @@
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -9,61 +8,76 @@ public class matrix_functionsTest {
 	@Test
 	public void testGetRow() {
 		// initialize new matrix
-		double[][] matrix_functions = new double[3][3];
+		matrix_functions matrixTest = new matrix_functions(3);
 		
 		// Insert values into new matrix
-		matrix_functions[0][0] = 5;
-		matrix_functions[0][1] = 4;
-		matrix_functions[0][2] = 3;
-		matrix_functions[1][0] = 6;
-		matrix_functions[1][1] = 9;
-		matrix_functions[1][2] = 1;
-		matrix_functions[2][0] = 2;
-		matrix_functions[2][1] = 5;
-		matrix_functions[2][2] = 3;
+		matrixTest.matrixA[0][0] = 5.0;
+		matrixTest.matrixA[0][1] = 4.0;
+		matrixTest.matrixA[0][2] = 3.0;
+		matrixTest.matrixA[1][0] = 6.0;
+		matrixTest.matrixA[1][1] = 9.0;
+		matrixTest.matrixA[1][2] = 1.0;
+		matrixTest.matrixA[2][0] = 2.0;
+		matrixTest.matrixA[2][1] = 5.0;
+		matrixTest.matrixA[2][2] = 3.0;
 		
 		// Test is the row is output correctly
-		assertEquals(82.0, example.matrix_functions.getRow(3), 0.001);
+		assertTrue(matrixTest.getRow(0).equals("5.0 4.0 3.0"));
 	}
 
 	@Test
 	public void testFindDeterminant() {
 		// initialize new matrix
-		double[][] matrix_functions = new double[3][3];
+		matrix_functions matrixTest = new matrix_functions(3);
 		
 		// Insert values into new matrix
-		matrix_functions[0][0] = 5;
-		matrix_functions[0][1] = 4;
-		matrix_functions[0][2] = 3;
-		matrix_functions[1][0] = 6;
-		matrix_functions[1][1] = 9;
-		matrix_functions[1][2] = 1;
-		matrix_functions[2][0] = 2;
-		matrix_functions[2][1] = 5;
-		matrix_functions[2][2] = 3;
+		matrixTest.matrixA[0][0] = 5.0;
+		matrixTest.matrixA[0][1] = 4.0;
+		matrixTest.matrixA[0][2] = 3.0;
+		matrixTest.matrixA[1][0] = 6.0;
+		matrixTest.matrixA[1][1] = 9.0;
+		matrixTest.matrixA[1][2] = 1.0;
+		matrixTest.matrixA[2][0] = 2.0;
+		matrixTest.matrixA[2][1] = 5.0;
+		matrixTest.matrixA[2][2] = 3.0;
 				
 		// Test if determinant is 82
-		assertEquals(82.0, example.matrix_functions.determinant(3, matrix_functions), 0.001);
+		assertEquals(82.0, matrixTest.findDeterminant(), 0.1);
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testFindInverse() {
 		// initialize new matrix
-		double[][] matrix_functions = new double[3][3];
+		matrix_functions matrixTest = new matrix_functions(3);
 				
 		// Insert values into new matrix
-		matrix_functions[0][0] = 5;
-		matrix_functions[0][1] = 4;
-		matrix_functions[0][2] = 3;
-		matrix_functions[1][0] = 6;
-		matrix_functions[1][1] = 9;
-		matrix_functions[1][2] = 1;
-		matrix_functions[2][0] = 2;
-		matrix_functions[2][1] = 5;
-		matrix_functions[2][2] = 3;
+		matrixTest.matrixA[0][0] = 2.0;
+		matrixTest.matrixA[0][1] = 1.0;
+		matrixTest.matrixA[0][2] = 1.0;
+		matrixTest.matrixA[1][0] = -5.0;
+		matrixTest.matrixA[1][1] = -3.0;
+		matrixTest.matrixA[1][2] = 0.0;
+		matrixTest.matrixA[2][0] = 1.0;
+		matrixTest.matrixA[2][1] = 1.0;
+		matrixTest.matrixA[2][2] = -1.0;
+		
+		// initialize new matrix
+		matrix_functions matrixTestB = new matrix_functions(3);
+		
+		// Insert values into new matrix
+		matrixTestB.matrixA[0][0] = -3.0;
+		matrixTestB.matrixA[0][1] = -2.0;
+		matrixTestB.matrixA[0][2] = -3.0;
+		matrixTestB.matrixA[1][0] = 5.0;
+		matrixTestB.matrixA[1][1] = 3.0;
+		matrixTestB.matrixA[1][2] = 5.0;
+		matrixTestB.matrixA[2][0] = 2.0;
+		matrixTestB.matrixA[2][1] = 1.0;
+		matrixTestB.matrixA[2][2] = 1.0;
 		
 		// Test is the row is output correctly
-		assertEquals(82.0, example.matrix_functions.findInverse(3, matrix_functions), 0.001);
+		assertEquals(matrixTestB.matrixA, matrixTest.findInverse().matrixA);
 	}
 
 }
